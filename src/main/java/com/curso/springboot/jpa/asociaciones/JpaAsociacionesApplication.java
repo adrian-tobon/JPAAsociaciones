f3d48a2d7c86c49c5ddb9ac27e63a8151bd35c3c
Carlos Adrian Tobon Perez <catobon@personalsoft.com>
2026-01-06 11:44:39 -0500

    relacion Many to One JPA 3

--- src/main/java/com/curso/springboot/jpa/asociaciones/JpaAsociacionesApplication.java
+++ src/main/java/com/curso/springboot/jpa/asociaciones/JpaAsociacionesApplication.java
@@ -1,5 +1,9 @@
 package com.curso.springboot.jpa.asociaciones;
 
+import java.util.Optional;
+import java.util.Scanner;
+
+import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -28,7 +32,10 @@ public class JpaAsociacionesApplication implements CommandLineRunner {
 	@SuppressWarnings("null")
 	@Override
 	public void run(String... args) throws Exception {
-		manyToOne();
+		// manyToOne();
+		// findClientById();
+		
+		createInvoice();
 	}
 	
 	@Transactional
@@ -46,4 +53,66 @@ public class JpaAsociacionesApplication implements CommandLineRunner {
 		
 	}
 
+	
+	@Transactional
+	public void findClientById() {
+	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("----------Busqueda de cliente---------------");
+		System.out.println("Ingrese el id del cliente:");
+		Long id = scanner.nextLong();
+		
+		Optional<Client> optClient = clientRepository.findById(id);
+		
+		/*optClient.ifPresentOrElse(c -> {
+				System.out.println(c);
+			}, () -> {
+				System.out.println("cliente no existe");
+		});*/
+		
+		if(optClient.isPresent()) {
+			Client client = optClient.orElseThrow();
+			System.out.println(client);
+		}else {
+			System.out.println("cliente no existe");
+		}
+		
+		scanner.close();
+	
+	}
+	
+	@Transactional
+	public void createInvoice() {		
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("----------Busqueda de cliente---------------");
+		System.out.println("Ingrese el id del cliente:");
+		Long id = scanner.nextLong();
+		
+		Optional<Client> optClient = clientRepository.findById(id);	
+		
+		if(optClient.isPresent()) {
+			Client client = optClient.orElseThrow();
+			System.out.println(client);
+			System.out.println("----------Creacion de factura---------------");
+			System.out.println("Ingrese la descripcion de la factura:");
+ 			scanner.nextLine();
+ 			String description = scanner.nextLine();
+ 			System.out.println("Ingrese el total de la factura:");
+ 			Long total = scanner.nextLong();
+			
+			Invoice invoice = new Invoice(description,total);
+			invoice.setClient(client);
+			
+			Invoice invoiceDB = invoiceRepository.save(invoice);
+			System.out.println("Factura creada");
+			System.out.println(invoiceDB);
+			
+		}else {
+			System.out.println("Cliente no existe");
+		}
+		
+		scanner.close();
+		
+	}
 }
--- src/main/resources/queries/queries.sql
+++ src/main/resources/queries/queries.sql
@@ -1,8 +1,18 @@
 create database db_jpa_relationship;
 
+create table db_jpa_relationship.clients (id bigint not null auto_increment, lastname varchar(255), name varchar(255), primary key (id)) engine=InnoDB;
+create table db_jpa_relationship.invoices (client_id bigint, id bigint not null auto_increment, total bigint, description varchar(255), primary key (id)) engine=InnoDB;
+alter table db_jpa_relationship.invoices add constraint fk_clientid foreign key (client_id) references clients (id);
 
-insert into clients (name, lastname) values ('Pepe','Doe');
-insert into clients (name, lastname) values ('Luis','Perez');
+insert into db_jpa_relationship.clients (name, lastname) values ('Pepe','Doe');
+insert into db_jpa_relationship.clients (name, lastname) values ('Luis','Perez');
+insert into db_jpa_relationship.clients (name, lastname) values ('Jorge','Perez');
+insert into db_jpa_relationship.clients (name, lastname) values ('Juan','Garcia');
+insert into db_jpa_relationship.clients (name, lastname) values ('Jose','Artua');
+insert into db_jpa_relationship.clients (name, lastname) values ('Leon','Gomez');
+insert into db_jpa_relationship.clients (name, lastname) values ('Ian','Neeson');
+insert into db_jpa_relationship.clients (name, lastname) values ('Lan','Caster');
+
 
 select * from db_jpa_relationship.invoices;
 
