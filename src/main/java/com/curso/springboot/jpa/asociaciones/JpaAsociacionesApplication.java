bd3a0007bb98d34b7f52fbaac86c071298fa961b
Carlos Adrian Tobon Perez <catobon@personalsoft.com>
2026-01-06 08:08:31 -0500

    first commit

---
+++ src/main/java/com/curso/springboot/jpa/asociaciones/JpaAsociacionesApplication.java
@@ -0,0 +1,13 @@
+package com.curso.springboot.jpa.asociaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAsociacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAsociacionesApplication.class, args);
+	}
+
+}
